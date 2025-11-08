26634686c2fccac97abc5082eddf865101aafb46
Vishakha Kesare <vishakha.kesare@delaplex.com>
2025-11-08 22:40:10 +0530

    create DBContext

---
+++ WebApi/DBContext/HMSDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.DBContext
+{
+    public class HMSDBContext: DbContext
+    {
+        public HMSDBContext(DbContextOptions<HMSDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Entities.BaseUserEntity> Users { get; set; }
+    }
+}
